0baf9f29d70c525f6ce4871a15621052f72255eb
houtian <houtian2015@sina.com>
2017-02-18 22:53:57 +0800

    Add account activations

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Mail;
 
 class UsersController extends Controller
 {
@@ -81,9 +82,9 @@ class UsersController extends Controller
             'password' => bcrypt($request->password)
         ]);
 
-        Auth::login($user);
-        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
-        return redirect()->route('users.show', [$user]);
+        $this->sendEmailConfirmationTo($user);
+        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收');
+        return redirect('/');
 
         return;
     }
@@ -96,5 +97,32 @@ class UsersController extends Controller
 
         session()->flash('success', '成功删除用户');
         return back();
+    }
+
+    public function confirmEmail($token)
+    {
+        $user = User::where('activation_token', $token)->firstOrFail();
+
+        $user->activated = true;
+        $user->activation_token = null;
+        $user->save();
+
+        Auth::login($user);
+        session()->flash('success', '恭喜你，激活成功！');
+        return redirect()->route('users.show', [$user]);
+    }
+
+    protected function sendEmailConfirmationTo($user)
+    {
+        $view = 'emails.confirm';
+        $data = compact('user');
+        $from = 'houtian2015@sina.com';
+        $name = 'houtian';
+        $to = $user->email;
+        $subject = "感谢注册 Sample 应用! 请确认您的邮箱.";
+
+        Mail::send($view, $data, function($message) use ($from, $name, $to, $subject) {
+            $message->from($from, $name)->to($to)->subject($subject);
+        });
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -27,3 +27,5 @@ Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destory')->name('logout');
 
 Route::get('/user/{id}/edit', 'UsersController@edit')->name('users.edit');
+
+Route::get('/signup/confirm/{token}', 'UsersController@confirmEmail')->name('confirm_email');
